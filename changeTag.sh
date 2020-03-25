4ab89c707f8a91713474a74bb7221136f471ed27
Rodrigo Fuenzalida <rodrigo.fuenzalida@coopeuch.cl>
2020-03-25 10:29:57 -0300

    update file

--- changeTag.sh
+++ changeTag.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sed "s/tagVersion/$1/g" pods.yaml > app.yaml
-sed "s/repositoryUrl/$2/g" app.yaml > app.yaml
+sed "s/tagVersion/$1/g" pods.yaml > app1.yaml
+sed "s,repositoryUrl,$2,g" app1.yaml > app.yaml
+rm app1.yaml
